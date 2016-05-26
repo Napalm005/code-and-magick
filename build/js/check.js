c4d982f7ab1df62221a8426190992c30adda6b02
Napalm005 <Napalm_08@mail.ru>
2016-05-26 22:34:10 +0300

    #5 Начинаем программировать

---
+++ build/js/check.js
@@ -0,0 +1,25 @@
+function getMessage(a, b) {
+    if (typeof a === "boolean") {
+        if (a == true) {
+            return "Я попал в " + b;
+        } else {
+            return "Я никуда не попал";
+        }
+    } else if (typeof a === "number") {
+        return "Я прыгнул на " + a * 100 + " сантиметров";
+    } else if (typeof a === "object" && typeof b === "object") {
+        var length = 0;
+        for (var i = 0; i < a.length; i++) {
+            var c = a[i] * b[i];
+            length += c;
+        }
+        return "Я прошёл " + length + " метров";
+    } else if (typeof a === "object") {
+        var sum = 0;
+        for (var i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return "Я прошёл " + sum + " шагов";
+    }
+};
+
