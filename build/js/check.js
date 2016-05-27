ca6fe7e6972af2692d8b5abfdcaacf7861655122
Napalm005 <Napalm_08@mail.ru>
2016-05-27 09:23:59 +0300

    Добавил функцию Array.isArray(argument)

--- build/js/check.js
+++ build/js/check.js
@@ -1,20 +1,20 @@
 function getMessage(a, b) {
     if (typeof a === "boolean") {
-        if (a == true) {
+        if (a) {
             return "Я попал в " + b;
         } else {
             return "Я никуда не попал";
         }
     } else if (typeof a === "number") {
         return "Я прыгнул на " + a * 100 + " сантиметров";
-    } else if (typeof a === "object" && typeof b === "object") {
+    } else if ( (Array.isArray(a)) && (Array.isArray(b)) ) {
         var length = 0;
         for (var i = 0; i < a.length; i++) {
             var c = a[i] * b[i];
             length += c;
         }
         return "Я прошёл " + length + " метров";
-    } else if (typeof a === "object") {
+    } else if (Array.isArray(a)) {
         var sum = 0;
         for (var i = 0; i < a.length; i++) {
             sum += a[i];
