304aa0804aacbfd02ce43d8b7e8e4215f2b8c656
Napalm005 <Napalm_08@mail.ru>
2016-05-27 12:01:06 +0300

    перегруппированы условия на массивы

--- build/js/check.js
+++ build/js/check.js
@@ -8,18 +8,20 @@ function getMessage(a, b) {
         }
     } else if (typeof a === "number") {
         result = "Я прыгнул на " + a * 100 + " сантиметров";
-    } else if ((Array.isArray(a)) && (Array.isArray(b))) {
-        var length = 0;
-        for (var i = 0; i < a.length; i++) {
-            length += a[i] * b[i];
+    } else if (Array.isArray(a)) {
+        if (Array.isArray(b)) {
+            var length = 0;
+            for (var i = 0; i < a.length; i++) {
+                length += a[i] * b[i];
+            }
+            result = "Я прошёл " + length + " метров";
+        } else {
+            var sum = 0;
+            for (var i = 0; i < a.length; i++) {
+                sum += a[i];
+            }
+            result = "Я прошёл " + sum + " шагов";
         }
-        result = "Я прошёл " + length + " метров";
-    } else {
-        var sum = 0;
-        for (var i = 0; i < a.length; i++) {
-            sum += a[i];
-        }
-        result = "Я прошёл " + sum + " шагов";
     }
     return result;
 };
