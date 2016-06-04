80dd87f3d8ee3bfeba47ad8a79f3255094cd4fc6
Napalm005 <Napalm_08@mail.ru>
2016-06-04 17:30:07 +0300

    Влидация формы. Начало

--- src/form.js
+++ src/form.js
@@ -4,6 +4,67 @@
   var formContainer = document.querySelector('.overlay-container');
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
+  var form = document.querySelector('.review-form');
+  var formReviewName = form.elements['review-name'];
+  var formReviewText = form.elements['review-text'];
+  var formReviewMarks = form.elements['review-mark'];
+  var formNameLink = form.querySelector('.review-form-controls .review-fields-name');
+  var formTextLink = form.querySelector('.review-form-controls .review-fields-text');
+  var formReviewControl = form.querySelector('.review-fields');
+  var formButton = form.querySelector('.review-submit');
+
+  formReviewName.required = true;
+
+  form.oninput = function() {
+    defineReviewTextRequire();
+    hideLinksTips();
+    disableButton();
+  }
+
+  /**
+   * Обязует заполнять поле отзыва при оценке ниже 3.
+   */
+  function defineReviewTextRequire() {
+    if (formReviewMarks.value < 3) {
+      formReviewText.required = true;
+    } else {
+      formReviewText.required = false;
+    }
+  }
+
+  /**
+   * Прячет подсказки при заполнении полей.
+   */
+  function hideLinksTips() {
+    if (formReviewName.value !== '') {
+      formNameLink.classList.add('invisible');
+    } else {
+      formNameLink.classList.remove('invisible');
+    }
+
+    if (formReviewText.value !== '') {
+      formTextLink.classList.add('invisible');
+    } else {
+      formTextLink.classList.remove('invisible');
+    }
+
+    if ( (formReviewText.value !== '') && (formReviewName.value !== '') ) {
+      formReviewControl.classList.add('invisible');
+    } else {
+      formReviewControl.classList.remove('invisible');
+    }
+  }
+
+  /**
+   * Делает кнопку неактивной, пока форма невалидна.
+   * (Пока не работает)
+   */
+  function disableButton() {
+    while (form.submit = false) {
+      formButton.disabled = true;
+    }
+  }
+
 
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
