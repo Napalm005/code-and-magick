a7936c0c5042121a5726f074030cf072a35a806b
Napalm005 <Napalm_08@mail.ru>
2016-06-11 15:45:07 +0300

    Константа большими буквами. Ф-ция переименована и разбита на части. Сократил кол-во вызовов поиска эл-тов. Убрал return. Нов. ф-ция.

--- src/reviews.js
+++ src/reviews.js
@@ -2,42 +2,60 @@
 
 (function() {
   var reviewsFilterBlock = document.querySelector('.reviews-filter');
-  var templateElement = document.querySelector('template');
   var elementToClone;
-  var invisible = 'invisible';
+  var reviewsContainer = document.querySelector('.reviews-list');
+  var INVISIBLE = 'invisible';
 
-  reviewsFilterBlock.classList.add(invisible);
-
-  if ('content' in templateElement) {
-    elementToClone = templateElement.content.querySelector('.review');
-  } else {
-    elementToClone = templateElement.querySelector('.review');
-  }
+  reviewsFilterBlock.classList.add(INVISIBLE);
+  supportTemplateTag();
 
   /** @constant {number} */
   var IMAGE_LOAD_TIMEOUT = 5000;
 
   /**
+    * Проверяет поддержку элемента template и получает в нём контент.
+    * @param {Object} data
+    * @param {HTMLElement} container
+    */
+  function supportTemplateTag() {
+    var templateElement = document.querySelector('template');
+
+    if ('content' in templateElement) {
+      elementToClone = templateElement.content.querySelector('.review');
+    } else {
+      elementToClone = templateElement.querySelector('.review');
+    }
+  }
+  /**
     * Клонирует элемент из шаблона, подставляет данные из объекта на сервере
     * и рендерит элемент в указанный блок.
     * @param {Object} data
     * @param {HTMLElement} container
-    * @return {HTMLElement}
     */
-  var getHotelElement = function(data, container) {
+  var cloneAndRenderReviewElements = function(data, container) {
     var element = elementToClone.cloneNode(true);
     element.querySelector('.review-text').textContent = data.description;
     element.querySelector('.review-rating').textContent = data.rating;
     container.appendChild(element);
+    setImageParameters(data, element);
+  };
 
+  /**
+    * Создаёт изображения, которые получают необходимые параметры из
+    * свойств объекта на сервере и добавляет им обработчики загрузки и ошибки.
+    * @param {Object} data
+    * @param {HTMLElement} element
+    */
+  function setImageParameters(data, element) {
     var image = new Image(124, 124);
     var imageLoadTimeout;
 
     image.onload = function() {
       clearTimeout(imageLoadTimeout);
-      element.querySelector('.review-author').src = image.src;
-      element.querySelector('.review-author').width = image.width;
-      element.querySelector('.review-author').height = image.height;
+      var reviewAvatar = element.querySelector('.review-author');
+      reviewAvatar.src = image.src;
+      reviewAvatar.width = image.width;
+      reviewAvatar.height = image.height;
     };
 
     image.onerror = function() {
@@ -50,15 +68,11 @@
       image.src = '';
       element.classList.add('review-load-failure');
     }, IMAGE_LOAD_TIMEOUT);
-
-    return element;
-  };
-
+  }
 
   window.reviews.forEach(function(review) {
-    var reviewsContainer = document.querySelector('.reviews-list');
-    getHotelElement(review, reviewsContainer);
+    cloneAndRenderReviewElements(review, reviewsContainer);
   });
 
-  reviewsFilterBlock.classList.remove(invisible);
+  reviewsFilterBlock.classList.remove(INVISIBLE);
 })();
