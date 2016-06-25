9e5e3eca9e60fef1b5038878942fe8e1da856dc4
Napalm005 <Napalm_08@mail.ru>
2016-06-25 13:23:58 +0300

    Создан конструктор Variables

--- src/variables.js
+++ src/variables.js
@@ -2,43 +2,45 @@
 
 define(function() {
 
-  return {
-    moreReviewsButton: document.querySelector('.reviews-controls-more'),
-      /** @type {Array.<Object>} */
-    filteredReviews: [],
+  function Variables() {
+    this.moreReviewsButton = document.querySelector('.reviews-controls-more');
     /** @type {Array.<Object>} */
-    reviews: [],
+    this.filteredReviews = [];
+    /** @type {Array.<Object>} */
+    this.reviews = [];
     /** @type {number} */
-    currentOffset: 0,
+    this.currentOffset = 0;
     /** @enum {string} */
-    FILTER: {
+    this.FILTER = {
       'ALL': 'reviews-all',
       'RECENT': 'reviews-recent',
       'GOOD': 'reviews-good',
       'BAD': 'reviews-bad',
       'POPULAR': 'reviews-popular'
-    },
-    ratingClasses: [
+    };
+    this.ratingClasses = [
       'review-rating',
       'review-rating-two',
       'review-rating-three',
       'review-rating-four',
       'review-rating-five'
-    ],
-    reviewsContainer: document.querySelector('.reviews-list'),
-    reviewsFilterBlock: document.querySelector('.reviews-filter'),
+    ];
+    this.reviewsContainer = document.querySelector('.reviews-list');
+    this.reviewsFilterBlock = document.querySelector('.reviews-filter');
     /** @constant {Filter} */
-    DEFAULT_FILTER: this.FILTER.ALL,
+    this.DEFAULT_FILTER = this.FILTER.ALL;
     /** @constant {number} */
-    LOAD_TIMEOUT: 5000,
+    this.LOAD_TIMEOUT = 5000;
     /** @constant {string} */
-    CLASS_INVISIBLE: 'invisible',
+    this.CLASS_INVISIBLE = 'invisible';
     /** @constant {string} */
-    CLASS_REVIEWS_SECTION_FAILURE: 'reviews-load-failure',
+    this.CLASS_REVIEWS_SECTION_FAILURE = 'reviews-load-failure';
     /** @constant {string} */
-    CLASS_REVIEWS_SECTION_LOADING: 'reviews-list-loading',
+    this.CLASS_REVIEWS_SECTION_LOADING = 'reviews-list-loading';
     /** @constant {number} */
-    LIMIT: 3
-  };
+    this.LIMIT = 3;
+  }
+
+  return new Variables();
 });
 
