732974cf4d80ac4087118b570626eae1897e0e8d
Napalm005 <Napalm_08@mail.ru>
2016-06-25 13:34:03 +0300

    Переназначил this на self

--- src/variables.js
+++ src/variables.js
@@ -2,43 +2,46 @@
 
 define(function() {
 
+
   function Variables() {
-    this.moreReviewsButton = document.querySelector('.reviews-controls-more');
+    var self = this;
+
+    self.moreReviewsButton = document.querySelector('.reviews-controls-more');
     /** @type {Array.<Object>} */
-    this.filteredReviews = [];
+    self.filteredReviews = [];
     /** @type {Array.<Object>} */
-    this.reviews = [];
+    self.reviews = [];
     /** @type {number} */
-    this.currentOffset = 0;
+    self.currentOffset = 0;
     /** @enum {string} */
-    this.FILTER = {
+    self.FILTER = {
       'ALL': 'reviews-all',
       'RECENT': 'reviews-recent',
       'GOOD': 'reviews-good',
       'BAD': 'reviews-bad',
       'POPULAR': 'reviews-popular'
     };
-    this.ratingClasses = [
+    self.ratingClasses = [
       'review-rating',
       'review-rating-two',
       'review-rating-three',
       'review-rating-four',
       'review-rating-five'
     ];
-    this.reviewsContainer = document.querySelector('.reviews-list');
-    this.reviewsFilterBlock = document.querySelector('.reviews-filter');
+    self.reviewsContainer = document.querySelector('.reviews-list');
+    self.reviewsFilterBlock = document.querySelector('.reviews-filter');
     /** @constant {Filter} */
-    this.DEFAULT_FILTER = this.FILTER.ALL;
+    self.DEFAULT_FILTER = self.FILTER.ALL;
     /** @constant {number} */
-    this.LOAD_TIMEOUT = 5000;
+    self.LOAD_TIMEOUT = 5000;
     /** @constant {string} */
-    this.CLASS_INVISIBLE = 'invisible';
+    self.CLASS_INVISIBLE = 'invisible';
     /** @constant {string} */
-    this.CLASS_REVIEWS_SECTION_FAILURE = 'reviews-load-failure';
+    self.CLASS_REVIEWS_SECTION_FAILURE = 'reviews-load-failure';
     /** @constant {string} */
-    this.CLASS_REVIEWS_SECTION_LOADING = 'reviews-list-loading';
+    self.CLASS_REVIEWS_SECTION_LOADING = 'reviews-list-loading';
     /** @constant {number} */
-    this.LIMIT = 3;
+    self.LIMIT = 3;
   }
 
   return new Variables();
